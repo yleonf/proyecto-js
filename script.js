fcf446686eadf1d3454f30e0c765235d65854bb1
yleonf <yasnaleonfoitzick@gmail.com>
2025-05-08 13:37:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+ const students=[]
+
+ document.getElementById("studentForm").addEventListener("submit",function (e){
+    e.preventDefault();
+    
+    const name=document.getElementById("name").value.trim();
+    const lastName=document.getElementById("lastName").value.trim();
+    const grade=parseFloat(document.getElementById("grade").value);
+
+    if(grade>7 || grade<1 || !name || ! lastName || isNaN(grade)){
+        alert("Error al ingresar los datos")
+            return
+    }
+     const student={name,lastName,grade}
+     students.push(student)
+     console.log(students)
+ addStudentToTable(student)
+      this.reset();
+
+ })
+const tableBody=document.querySelector("#studentTable tbody")
+ function addStudentToTable(student){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+    <td>${student.name}</td>
+    <td>${student.lastName}</td>
+    <td>${student.grade}</td>
+    `;
+tableBody.appendChild(row)
+ }
